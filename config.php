0234880aa0ffd1bb8474764d11c3570352dbc399
bbrinston <39634310+bbrinston@users.noreply.github.com>
2018-07-28 15:48:32 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+ini_set('display_errors', 'On');
+
+define ("DB_NAME", "database45");
+define ("DB_USERNAME", "root");
+define ("DB_PASSWORD", "");
+define ("DB_HOST", "localhost");
+
+define ("IMAGE_DIRECTORY", "../images");
+
+
+?>

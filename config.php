ec75c5013f98046bf9bd651736b16c266e7cf0de
bbrinston <39634310+bbrinston@users.noreply.github.com>
2018-07-29 12:17:14 -0400

    d

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 ini_set('display_errors', 'On');
 
 define ("DB_NAME", "database45");
-define ("DB_USERNAME", "root");
-define ("DB_PASSWORD", "pass123?");
-define ("DB_HOST", "den1.mssql1.gear.host");
+define ("DB_USERNAME", "testlook\$testlook");
+define ("DB_PASSWORD", "nuKR4uxN82yx3lgYp5EbpnZKhz2mmysENZlhedaZWftqWkyhADH3B8DQAY0f");
+define ("DB_HOST", "ftp.gear.host");
 
 define ("IMAGE_DIRECTORY", "../images");
 

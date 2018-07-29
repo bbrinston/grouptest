7794ad316c92cbe186622c6d69f3617d03e7a21a
bbrinston <39634310+bbrinston@users.noreply.github.com>
2018-07-29 12:43:41 -0400

    again

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 ini_set('display_errors', 'On');
 
-define ("DB_NAME", "database45");
-define ("DB_USERNAME", "database45");
+define ("DB_NAME", "database46");
+define ("DB_USERNAME", "database46");
 define ("DB_PASSWORD", "pass123?");
-define ("DB_HOST", "den1.mssql1.gear.host");
+define ("DB_HOST", "den1.mysql4.gear.host");
 
 
 ?>

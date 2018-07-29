2b88245d60b5ca7ac74b2a7f8179357edc8bbc3b
bbrinston <39634310+bbrinston@users.noreply.github.com>
2018-07-29 12:14:21 -0400

    config

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 'On');
 
 define ("DB_NAME", "database45");
 define ("DB_USERNAME", "root");
-define ("DB_PASSWORD", "Dar789win654");
+define ("DB_PASSWORD", "pass123?");
 define ("DB_HOST", "den1.mssql1.gear.host");
 
 define ("IMAGE_DIRECTORY", "../images");
